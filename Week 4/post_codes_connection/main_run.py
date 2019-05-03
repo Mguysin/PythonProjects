6f8038f6767b8001b3f9e5e385903a83a2b56d65
Marat Guysin <mguysin17@gmail.com>
2019-05-03 11:34:05 +0100

    update

---
+++ Week 4/post_codes_connection/main_run.py
@@ -0,0 +1,2 @@
+from import post_codes_connection
+
--- Week 4/post_codes_connection/post_codes_connection.py
+++ Week 4/post_codes_connection/post_codes_connection.py
@@ -1,25 +1,60 @@
 import requests
 
 class Postcode:
-    def __init__(self, postcode):
+    def __init__(self, postcode, status, result):
         self.postcode = postcode
+        self.status = status
+        self.result = result
 
-    def look_up_one_postcode(self,pcode):
+        #you need to have a ststus and result
+
+    def look_up_one_postcode(self, post_codes):
         post_codes = requests.get('https://api.postcodes.io/postcodes/E146HS')
+        # not hardoce your post code
+        #access the result of the query
+            # save the status to the attributes of the objct
+            # save the result to the attributes of the objct
 
-    def Print_region_country(self):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         print(post_codes)
-
-    def Print_all_details(self):
         for key in post_codes.json()['result']:
             print('key:', key)
             print('value:', post_codes.json()['result'][key])
+
+    look_up_one_postcode()
+    print(look_up_one_postcode())
+
+
+    def Print_region_country(self):
+        pass
+
+    def Print_all_details(self):
+        pass
 
     def Latitude_long_Print(self):
         pass
 
         post_codes = requests.get('https://api.postcodes.io/postcodes/E146HS')
-
         print(post_codes)
         print(type(post_codes.json()))
         print(post_codes.json().keys())
@@ -27,3 +62,5 @@ class Postcode:
         for key in post_codes.json()['result']:
             print('key:', key)
             print('value:', post_codes.json()['result'][key])
+
+
